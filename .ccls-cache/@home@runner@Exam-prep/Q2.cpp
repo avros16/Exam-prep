328573ce6c910a51a4cd09c0b035b7ddaf075765
avros16 <114757828+avros16@users.noreply.github.com>
2024-05-20 02:00:17 +0000

    Q2

---
+++ .ccls-cache/@home@runner@Exam-prep/Q2.cpp
@@ -0,0 +1,18 @@
+// #include <iostream>
+// using namespace std;
+
+
+// // 1. Write a program that calculate the sum of all elements in an integer array
+
+// int main() {
+// int sum {0};
+
+
+//   int arr[5] = {1,2,3,4,5};
+
+
+//   for (int i = 0; i<5; i++){
+//     sum += arr[i];
+//   }
+//   cout << "This is the sum: " << sum;
+// }
--- .ccls-cache/@home@runner@Exam-prep/main.cpp
+++ .ccls-cache/@home@runner@Exam-prep/main.cpp
@@ -1,14 +1,18 @@
 #include <iostream>
 using namespace std;
 
-int main() {
 
+// 1. Write a program that calculate the sum of all elements in an integer array
+
+int main() {
+int sum {0};
 
   
   int arr[5] = {1,2,3,4,5};
 
   
   for (int i = 0; i<5; i++){
-    cout << arr[i] << endl;
+    sum += arr[i];
   }
+  cout << "This is the sum: " << sum;
 }
